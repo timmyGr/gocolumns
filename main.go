9124a1a70085279c3ec2c22f073be4b5e173ff04
timmyGr <yeowoh@gmail.com>
2020-04-03 10:47:03 -0500

    add padding to structs based on largest field name

--- main.go
+++ main.go
@@ -24,16 +24,33 @@ func main() {
 	_ = structString
 
 	colSlice := make([]string, 0)
+	maxChars := 0
 
 	for _, h := range header {
 		t := strings.ToTitle(strings.TrimSpace(h))
-		colSlice = append(colSlice, fmt.Sprintf("\t%s string `csv:\"%s\"` \n", t, t))
+		colSlice = append(colSlice, t)
+		// colSlice = append(colSlice, fmt.Sprintf("\t%s string `csv:\"%s\"` \n", t, t))
+
+		if len(t) > maxChars {
+			maxChars = len(t)
+		}
 	}
 
-	// pad struct values to properly line up when printing
+	for _, h := range colSlice {
+		if len(h) == maxChars {
+			structString += fmt.Sprintf("\t%s string `csv:\"%s\"` \n", h, h)
+		} else {
+			padding := ""
+
+			for i := 0; i < maxChars-len(h); i++ {
+				padding += " "
+			}
+
+			structString += fmt.Sprintf("\t%s %sstring `csv:\"%s\"` \n", h, padding, h)
+		}
+	}
 
 	structString += "}"
 
-	fmt.Println(colSlice)
 	fmt.Println(structString)
 }
